cf3bd3e4379092b1e19c7939b55fb7dd7d48e243
self067 <seltor@mail.ru>
2019-01-10 16:48:47 +0300

    10

--- content.php
+++ content.php
@@ -1,33 +1,32 @@
 <?php
 
-session_start();
+//session_start();
 
 if( !$_SESSION['login'] || !$_SESSION['pass']){
   header("Location: login.php");
   die();
 }
-if( $_POST['logout']){
+if(  isset( $_POST['logout'])){
   session_destroy();
   header('Location: login.php');
 }
 
+//setcookie('bg', $_POST['bg'], time()+120);
+$bg = $_SESSION['bgcolor'];
+
+//echo "<body bgcolor=#$bg>";
+
 ?>
 
-<body style="font-size: 40px;">
+<body style="font-size: 40px; background-color: <?php #$bg;?>">
 <p>Только для авторизированных пользователей</p>
-
 <?php
-echo "Привет, ". $_SESSION['login'] . "<br>"  ;
+echo "Привет, ". $_SESSION['login'] . "<br>" ;
 ?>
-
 <img src="8.jpg" alt="" width="600" style="display: block">
-
-
 <form method="POST" style="margin: 40px; font-size: 40px;">
-  <input style="font-size: 30px;" type="submit" name="logout" value="На страницу авторизации">
+  <input style="font-size: 30px;" type="submit" name="logout" value="Выход">
 </form>
-
-
 
 </body>
 
--- login.php
+++ login.php
@@ -2,26 +2,22 @@
 session_start();
 ini_set('session.gc.maxlifetime', 3600);
 
-if( $_SESSION['login'] && $_SESSION['pass']){
+if(  isset( $_SESSION['login'] ) && isset( $_SESSION['pass'] )){
   header("Location: content.php");
   die();
 }
 
-
-
-$connection = new PDO('mysql:host=jktu.ru; dbname=selto149_php; charset=utf8', 'selto149_php', 'AcademyPHP2@');
-$login = $connection->query('select * from `login`;');
-
-
-
-
-
-if( $_POST['login']){
+if( isset( $_POST['login'])){
+  $connection = new PDO('mysql:host=jktu.ru; dbname=selto149_php; charset=utf8', 'selto149_php', 'AcademyPHP2@');
+  $login = $connection->query('select * from `login`;');
   foreach ( $login as $log){
     if($_POST['login']== $log['user']  && $_POST['pass']== $log['pass']){
       $_SESSION['login'] = $_POST['login'];
       $_SESSION['pass'] = $_POST['pass'];
+      $_SESSION['bgcolor'] = $_POST['bg'];
+      setcookie('bg', $_POST['bg'], time()+360);
       header('Location: content.php');
+//      die();
     }
   }
   echo "Неверный логин или пароль";
@@ -31,11 +27,12 @@ if( $_POST['login']){
 ?>
   <style>
     body  {
-      margin: 200px 300px;
+      margin: 100px 300px;
     }
-    input, p {
+    input, p, select, span {
       font-size: 30px;
       margin: 10px;
+
     }
   </style>
 
@@ -43,7 +40,18 @@ if( $_POST['login']){
     <p>Авторизоваться</p>
     <input type="text" name="login" required placeholder="Логин"><br>
     <input type="password" name="pass" required placeholder="Пароль"><br>
-    <input type="submit">
+<!--      <span>Цвет фона</span>-->
+<!--      <select>-->
+<!--          <option>Желтый</option>-->
+<!--          <option>Зеленый</option>-->
+<!--          <option>Голубой</option>-->
+<!--      </select>-->
+
+      <p>Укажите цвет фона: <input type="color" name="bg" value="#ff0000"></p>
+<!--          <input type="submit" value="Выбрать">-->
+
+
+    <input type="submit" value="Вход">
   </form>
 <?php
 
